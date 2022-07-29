fec9dfb41450f126137d23fa598464cfae09b6e6
1095847440@qq.com <1095847440@qq.com>
2022-07-29 15:20:02 +0800

    first commit

---
+++ src/options/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+
+    主页面
+
+  </div>
+</template>
+
+<script>
+const chromeExtensionUrl = 'https://developer.chrome.com/docs/extensions/'
+export default {
+  name: 'App',
+
+  methods: {
+    goToExtensionDoc() {
+      window.open(chromeExtensionUrl)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import "../style/options.less";
+</style>
