0900b1b4eb12bc37af7aa5ff2de413e2d5628877
1095847440@qq.com <1095847440@qq.com>
2022-08-18 21:37:33 +0800

    adjust

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -2,7 +2,7 @@
   <div class="app">
     <!-- <iframe border="none" style="width: 375px;height: 580px;border: none;" :src="crxUrl" /> -->
 
- <iframe border="none" style="width: 375px;height: 580px;border: none;" src="../../assets/crx.html" />
+ <iframe border="none" style="width: 375px;height: 580px;border: none;" src="../../assets/index.html" />
 
     <!-- <iframe border="none" style="width: 375px;height: 580px;border: none;" src="https://static-59728804-d890-4267-8e45-393e10b3c780.bspapp.com/#/" /> -->
   </div>
