309b0fe04c0008c98a09e2369aafa545921e751d
1095847440@qq.com <1095847440@qq.com>
2022-09-03 23:28:07 +0800

    adjust

--- src/content/App.vue
+++ src/content/App.vue
@@ -56,7 +56,7 @@ export default {
             isOutAnim: 2,
             shade: false,
             shadeClose: false,
-            area: ['375px', '700px'],
+            area: ['375px', '500px'],
             // content: "https://static-59728804-d890-4267-8e45-393e10b3c780.bspapp.com/#/pages/test/addNote",
             content: crxUrl,
             end: () => {
